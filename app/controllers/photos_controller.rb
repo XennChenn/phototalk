dbea248d8673f9870fd711d21b1c9d32195b8529
Chenn <bxc_chenn@hotmail.com>
2016-08-30 17:57:24 +0800

    add photo controller and new

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,48 @@
+class PhotosController < ApplicationController
+  before_action :set_photo, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+
+  def index
+  	@photos = Photo.all
+  end
+
+  def show
+  end
+
+  def new
+  	@photo = current_user.photos.build
+  end
+
+  def create
+  	@photo = current_user.photos.build(photo_params)
+
+  	if @photo.save
+  		redirect_to photo_path(@photo)
+  	else
+  		render :new
+  	end
+  end
+
+  def edit
+  end
+
+  def update
+  	@photo.update(photo_params)
+  	redirect_to photo_path(@photo)
+  end
+
+  def destroy
+  	@photo.destroy
+  	redirect_to photos_path
+  end
+
+  private
+  def set_photo
+  	@photo = Photo.find(params[:id])
+  end
+
+  def photo_params
+  	params.require(:photo).permit(:pic, :description)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
 
+  resources :photos
+
   devise_for :users
 
-  devise_scope :user do
+ 
+
+  authenticated :user do
+    root "photos#index", as: "authenticated_root"
+  end
+
+   devise_scope :user do
     root to: "devise/sessions#new"
   end
   
